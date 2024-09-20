c5a74b20a6b4cd6986ea8fd0da56c997651d9e51
Martin Pizano M <mpizanom@gmail.com>
2024-09-20 10:38:55 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import UsuarioList from './components/Usuarios/UsuarioList';
+import EmpleadoList from './components/Empleados/EmpleadoList';
+import EstacionamientoList from './components/Estacionamientos/EstacionamientoList';
+import ReservaList from './components/Reservas/ReservaList';
+import LiberacionList from './components/Liberaciones/LiberacionList';
+import LogList from './components/Logs/LogList';
 import LoginForm, {} from "./componentes/LoginForm"
 
 function App() {
   return (
     <div className="App">
       <LoginForm></LoginForm>
+       <div className="App">
+        <div className="container">
+          <h1>Sistema de Gestión de Estacionamientos</h1>
+          <UsuarioList />
+          <EmpleadoList />
+          <EstacionamientoList />
+          <ReservaList />
+          <LiberacionList />
+          <LogList />
+        </div>
+      </div>
     </div>
   );
 }
