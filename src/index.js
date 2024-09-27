1204529f9e3d432c377c1d0a17832d70e4845c0f
Karina <129091450+karina725@users.noreply.github.com>
2024-09-27 11:24:06 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginForm, {} from "./componentes/LoginForm"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
