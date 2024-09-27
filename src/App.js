541be65035fc3776fedd73524d0464997abe4755
Karina <129091450+karina725@users.noreply.github.com>
2024-09-27 14:00:37 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import UsuarioList from './componentes/Usuarios/UsuarioList';
-import EmpleadoList from './componentes/Empleados/EmpleadoList';
-import EstacionamientoList from './componentes/Estacionamientos/EstacionamientoList';
-import ReservaList from './componentes/Reservas/ReservaList';
-import LiberacionList from './componentes/Liberaciones/LiberacionList';
-import LogList from './componentes/Logs/LogList';
 import LoginForm from "./componentes/LoginForm"; 
 
 function App() {
