9d6bc6ec184c3024ad7d11a501ede6ccaece282e
Karina <129091450+karina725@users.noreply.github.com>
2024-09-28 10:05:00 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import LoginForm from './componentes/LoginForm';
-
-
+import LoginForm from './componentes/LoginForm'; 
+import UsuarioList from './componentes/Usuarios/UsuarioList';
+import EmpleadoList from './componentes/Empleados/EmpleadoList';
+import EstacionamientoList from './componentes/Estacionamientos/EstacionamientoList';
+import ReservaList from './componentes/Reservas/ReservaList';
+import LiberacionList from './componentes/Liberaciones/LiberacionList';
+import LogList from './componentes/Logs/LogList';
 
 function App() {
   return (
-    <div className="login-container">
-        <h2>Iniciar Sesión</h2>
-        {/* Quita el resto del formulario por ahora */}
+    <div className="App">
+      <LoginForm />
+      <div className="container">
+        <h1>Sistema de Gestión de Estacionamientos</h1>
+        <UsuarioList />
+        <EmpleadoList />
+        <EstacionamientoList />
+        <ReservaList />
+        <LiberacionList />
+        <LogList />
+      </div>
     </div>
-);
+  );
+}
 
 export default App;
-
