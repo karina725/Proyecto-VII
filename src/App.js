194637fd9457c341ecdd76c6c7549e1a1d5aed96
Karina <129091450+karina725@users.noreply.github.com>
2024-09-28 11:14:38 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Route } from 'react-router-dom';
 
 import LoginForm from './componentes/LoginForm'; 
 import UsuarioList from './componentes/Usuarios/UsuarioList';
