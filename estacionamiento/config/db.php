872a2bedb041ac77a47c5ada8a9777a9d0240bcc
Karina <129091450+karina725@users.noreply.github.com>
2024-10-01 09:03:22 -0600

    Update db.php

--- estacionamiento/config/db.php
+++ estacionamiento/config/db.php
@@ -6,10 +6,6 @@ $dbname = 'dbs13342332';
 $username = 'root';
 $password = '';
 
-//$host = 'db5016421573.hosting-data.io';
-//$dbname = 'dbs13342332';
-//$username = 'dbu3737837';
-//$password = 'proyecto7Udg*karina';
 
 
 try {
