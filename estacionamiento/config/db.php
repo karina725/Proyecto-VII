cfe39f985b53d52aac16e5b2058337f5594a154c
Karina <129091450+karina725@users.noreply.github.com>
2024-10-01 09:03:36 -0600

    Update db.php

--- estacionamiento/config/db.php
+++ estacionamiento/config/db.php
@@ -6,8 +6,6 @@ $dbname = 'dbs13342332';
 $username = 'root';
 $password = '';
 
-
-
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
